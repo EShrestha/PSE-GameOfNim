a40052b790ce4d7a020d8b5a9ce503cfa33e9ea0
Erik Shrestha <Eshrestha@student.neumont.edu>
2021-01-18 10:24:53 -0700

    Finished USerInput and Board classes.

--- GameOfNim/src/Models/Board.java
+++ GameOfNim/src/Models/Board.java
@@ -4,7 +4,11 @@ public class Board {
 
     public int[][] board;
 
-    public void Board(byte rows, byte[] colAmounts){
+    public void Board(byte rows, int[] colAmounts){
         //TODO:Implement making a board with provided number of rows and each row has the specified colAmounts(just a number in this case)
+        board = new int[rows][];
+        for(int i=0; i<board.length; i++){
+            board[i][0] = colAmounts[i];
+        }
     }
 }
--- GameOfNim/src/Models/UserInput.java
+++ GameOfNim/src/Models/UserInput.java
@@ -1,21 +1,41 @@
 package Models;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class UserInput {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static String getString(){
-        //TODO:Implement getString
-        return "";
+    public static String getString(String prompt){
+        while(true){
+            System.out.print(prompt);
+            try{
+                return br.readLine();
+            } catch (IOException e){
+                System.err.println("***Something went wrong while getting user input***");
+            }
+        }
     }
 
-    public static int getInt(){
-        //TODO:Implement getInt
-        return 0;
+    public static int getInt(String prompt, int min, int max){
+        int tempInt;
+        while(true){
+            try {
+                tempInt = Integer.parseInt(getString(prompt));
+                if(tempInt >= min && tempInt <= max){return tempInt;}
+                else{ System.out.println("***Please enter numbers within bounds***");}
+            } catch (Exception e){System.out.println("***Please enter an integer***");}
+        }
     }
 
     public static int getInputFromMenu(String[] menuItems, boolean hasExit){
-        //TODO:Implement getInputFromMenu
-        return 0;
+        while(true){
+            for (String str : menuItems) {System.out.println(str);}
+            try { return getInt("Enter Selection: ", (hasExit ? 0 : 1), (hasExit?menuItems.length-1:menuItems.length));}
+            catch (Exception e){System.out.println("***Something went wrong with your selection, try again***");}
+        }
     }
 
 
