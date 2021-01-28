c02f637d8e7591219081e251df20dc63c095cf47
LordWrynn <44822503+LordWrynn@users.noreply.github.com>
2021-01-28 15:49:27 -0700

    Update Tester.java

--- GameOfNim/Tests/Tester.java
+++ GameOfNim/Tests/Tester.java
@@ -1,6 +1,14 @@
+import Models.Board;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Tester {
 
-    
+    @Test
+    public void testCheckRow() {
+        Board.setBoard(1);
+            assertEquals(6, Board.checkRow(2));
+    }
+
 }
