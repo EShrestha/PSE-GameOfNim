b4b5edd31661b856ae0515672498ecbe32c47e89
Erik Shrestha <Eshrestha@student.neumont.edu>
2021-01-28 21:02:46 -0700

    Added tests for getBoard and removePieces

--- GameOfNim/Tests/Tester.java
+++ GameOfNim/Tests/Tester.java
@@ -50,14 +50,60 @@ public class Tester {
     @Test
     public void testGetBoard() {
 
+        // Making easy board the active board
+        Board.setBoard(0); // 2x3 board
+        // Easy board should be 2 rows
+        assertEquals(2, Board.getBoard().length);
+        // Each row in easy board is 3 columns wide
+        for(boolean[] c : Board.getBoard()){assertEquals(3, c.length);}
+
+        // Making medium board the active board
+        Board.setBoard(1); // 3x7 board
+        // easy board should be 3 rows
+        assertEquals(3, Board.getBoard().length);
+        // Each row in medium board is 7 columns wide
+        for(boolean[] c : Board.getBoard()){assertEquals(7, c.length);}
+
+        // Making hard board the active board
+        Board.setBoard(2); // 4x9 board
+        // Hard board should be 4 rows
+        assertEquals(4, Board.getBoard().length);
+        // Each row in hard board is 9 columns wide
+        for(boolean[] c : Board.getBoard()){assertEquals(9, c.length);}
+
     }
     @Test
     public void testRemovePieces() {
+        Board.setBoard(0); // 2x3 board
+
+        // User cannot select a row that does not exist
+        assertFalse(Board.removePieces(new int[]{5,2}));
+
+        // User cannot select an amount that is greater than there are columns for the specified row
+        assertFalse(Board.removePieces(new int[]{2,9}));
+
+        // User cannot select a amount less than 1
+        assertFalse(Board.removePieces(new int[]{2, 0}));
+
+        Board.removePieces(new int[]{1,1}); // Simulating a player taking 1 of 3 pieces from row one
+
+        // User cannot take more pieces then there currently are on the specified row
+        assertFalse(Board.removePieces(new int[]{1, 3}));
+
+        // User can take all of the pieces from a row as long as there are more pieces in other rows
+        assertTrue(Board.removePieces(new int[]{1, 2}));
+
+        // User cannot take pieces from a row that has no pieces
+        assertFalse(Board.removePieces(new int[]{1, 2}));
+
+        // User cannot take all of the pieces form a row if said row is the only row on the board with pieces
+        assertFalse(Board.removePieces(new int[]{2, 3}));
+
 
     }
     @Test
     public void testIsGameOver() {
-        Board.setBoard(0);
+        Board.setBoard(0); // 2x3 board
 
         Board.removePieces(new int[]{1,3}); // Simulating a player taking 3 of 3 pieces from row one
         Board.removePieces(new int[]{2,1}); // Simulating a player taking 1 of 3 pieces from row two
@@ -74,7 +120,7 @@ public class Tester {
     }
     @Test
     public void testQuantityCheck() {
-        Board.setBoard(0);
+        Board.setBoard(0); // 2x3 board
 
         // User can take all 3 pieces from top row since there will be pieces on the second row
         assertTrue(Board.quantityCheck(0, 3));
